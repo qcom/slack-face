f917dd9b9c145766131f73a6252fa4e821797300
Zach Hodge <qcom100@gmail.com>
2016-05-26 08:26:36 -0700

    allow for setting left and right eye emojis for faces individually

--- src/lib/renderFace.js
+++ src/lib/renderFace.js
@@ -1,22 +1,31 @@
 import onlyEmojis from './onlyEmojis';
 import filterFalsy from './filterFalsy';
 
-export default function renderFace(options) {
+export default function renderFace(options={}) {
+
+	// allow options object to include 'eyes' or 'leftEye' + 'rightEye'
+	if (options.eyes) {
+		options.leftEye = options.eyes;
+		options.rightEye = options.eyes;
+	}
+
 	const defaults = {
-		eye:	':eye:',
+		leftEye:	':eye:',
+		rightEye:	':eye:',
 		nose:	':nose::skin-tone-2:',
 		mouth:	':lips:'
 	};
 
 	const filteredOptions = onlyEmojis(filterFalsy(options));
 
-	const { eye, nose, mouth } = Object.assign(defaults, filteredOptions);
+	const { leftEye, rightEye, nose, mouth } = Object.assign(defaults, filteredOptions);
 
 	const padding = '       ';
 
-	let face = `${eye} ${eye}`;
+	let face = `${leftEye} ${rightEye}`;
 	face += `\n${padding}${nose}`;
 	face += `\n${padding}${mouth}`;
 
 	return face;
+
 }
--- src/routers/post/middleware/buildFace.js
+++ src/routers/post/middleware/buildFace.js
@@ -1,12 +1,35 @@
 import renderFace from '../../../lib/renderFace';
 
 export default function buildFace(req, res, next) {
-	const options = req.argv.post;
-
 	// 'string' -> ['string'] if only 1 param
-	const [ eye, nose, mouth ] = [].concat(options);
+	const options = [].concat(req.argv.post);
 
-	const face = renderFace({ eye, nose, mouth });
+	let face;
+
+	switch (options.length) {
+		case 1: {
+			const [ eyes ] = options;
+			face = renderFace({ eyes });
+			break;
+		}
+		case 2: {
+			const [ eyes, nose ] = options;
+			face = renderFace({ eyes, nose });
+			break;
+		}
+		case 3: {
+			const [ eyes, nose, mouth ] = options;
+			face = renderFace({ eyes, nose, mouth });
+			break;
+		}
+		case 4: {
+			const [ leftEye, rightEye, nose, mouth ] = options;
+			face = renderFace({ leftEye, rightEye, nose, mouth });
+			break;
+		}
+		default:
+			face = renderFace();
+	}
 
 	req.face = face;
 
